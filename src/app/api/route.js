b5c2f8ec52da98229eb3582df6ff6f51b40e5557
GrowthMarketingDigital <bet7kgdm@gmail.com>
2024-03-05 12:53:27 -0300

    atualizações

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,27 +1,46 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
+let resend;
+
+try {
+  const apiKey = process.env.RESEND_API_KEY;
+  if (!apiKey) {
+    throw new Error("Chave de API não configurada. Verifique suas variáveis de ambiente.");
+  }
+
+  resend = new Resend(apiKey);
+} catch (error) {
+  console.error(error.message);
+  // Lidar com a falta da chave de API conforme necessário
+  // Aqui você pode fornecer um valor padrão para 'resend', ignorar o código, ou tomar outra ação apropriada.
+}
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
+
   try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
-    });
-    return NextResponse.json(data);
+    if (resend) {
+      const data = await resend.emails.send({
+        from: fromEmail,
+        to: [fromEmail, email],
+        subject: subject,
+        react: (
+          <>
+            <h1>{subject}</h1>
+            <p>Thank you for contacting us!</p>
+            <p>New message submitted:</p>
+            <p>{message}</p>
+          </>
+        ),
+      });
+      return NextResponse.json(data);
+    } else {
+      // Tratamento específico para quando 'resend' não está disponível (chave de API ausente)
+      return NextResponse.json({ error: "Erro de configuração: Chave de API ausente." });
+    }
   } catch (error) {
     return NextResponse.json({ error });
   }
